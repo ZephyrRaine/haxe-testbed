7980bb5a9660c05eb098a16bdd4ec64396403d04
ArthurLc <arthur.lacour-travail@laposte.net>
2023-02-11 15:35:48 +0100

    Planet: Random + Empty Functions.

--- src/game/PlanetState.hx
+++ src/game/PlanetState.hx
@@ -1,4 +1,5 @@
 import sample.SampleGame;
+import dn.Rand;
 
 enum abstract TILE_TYPE(Int) 
 {
@@ -15,23 +16,55 @@ enum abstract TILE_TYPE(Int)
 
 class PlanetState
 {
+    public var rand :Rand;
+
     public var startPosX : Int;
     public var startPosY : Int;
     public var planetSize : Int;
 
-    public function getTileType(cx:Int,cy:Int) : TILE_TYPE
-    {
-        return VIDE;
-    }
-
     public function new(_planetSize:Int)
     {
+        rand = new Rand(0);
+        rand.initSeed(Std.random(999999));
+
         planetSize = _planetSize;
         //REMPLIR START POS X ET Y
     }
+        
+    public inline function getTileType(cx:Int,cy:Int) : TILE_TYPE return VIDE;
 
     public function digCase(cx:Int, cy:Int)
     {
         cast(Game.ME, SampleGame).addGold(10);
+    }
+}
+
+private class InterestPoint
+{
+    private var chanceAppear : Int;
+    public inline function DoAppear(r : Rand) return r.irange(0,100) <= chanceAppear;
+    
+    private var distanceMin : Int;
+    private var distanceMax : Int;
+    public inline function GetRandomDistance(r : Rand) return r.irange(distanceMin, distanceMax);
+
+    public var chanceEarn : Float;
+    private var minEarn : Int;
+    private var maxEarn : Int;
+    public inline function GetRandomEarn(r : Rand) return r.irange(minEarn, maxEarn); 
+
+    public var chanceLostPA : Float;
+    public var losePA : Int;
+
+    public function new(chanceAppear : Int, distanceMin : Int, distanceMax : Int, chanceEarn : Float, minEarn : Int, maxEarn : Int, chanceLostPA : Float, losePA : Int)
+    {
+        this.chanceAppear = chanceAppear;
+        this.distanceMin = distanceMin;
+        this.distanceMax = distanceMax;
+        this.chanceEarn = chanceEarn;
+        this.minEarn = minEarn;
+        this.maxEarn = maxEarn;
+        this.chanceLostPA = chanceLostPA;
+        this.losePA = losePA;
     }
 }
